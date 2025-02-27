a3a925f727066bbefc5895e6e5786a14aa53f65e
AhmedAlshehhi7 <AhmedAlshehhi7@github.com>
2025-02-27 17:41:49 +0000

    Update external.mk to install assignment-4-test.sh for Assignment 4

--- base_external/external.mk
+++ base_external/external.mk
@@ -24,7 +24,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
 	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/writer $(TARGET_DIR)/usr/bin/writer
 	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
-	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-test.sh $(TARGET_DIR)/usr/bin/assignment-1-test.sh
+	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-test.sh $(TARGET_DIR)/usr/bin/assignment-4-test.sh
 	$(INSTALL) -D -m 0644 $(AESD_ASSIGNMENTS_DIR)/conf/username.txt $(TARGET_DIR)/etc/finder-app/username.txt
 	$(INSTALL) -D -m 0644 $(AESD_ASSIGNMENTS_DIR)/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/assignment.txt
 endef
