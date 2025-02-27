80e69d2d4a4464032584e9bb1a7ff5728887f8ab
AhmedAlshehhi7 <AhmedAlshehhi7@github.com>
2025-02-27 16:57:26 +0000

    Update external.mk to use local repository /home/ahmed/Assignment-2/finder-app

--- base_external/external.mk
+++ base_external/external.mk
@@ -4,10 +4,12 @@
 
 AESD_ASSIGNMENTS_VERSION = master
 
-# The SSH URL for your Assignment 3 repository (replace <username> with your GitHub username)
-AESD_ASSIGNMENTS_SITE = ssh://git@github.com/<username>/assignment-3-and-later.git
+# Use the local repository via file:// URL.
+# (Ensure that /home/ahmed/Assignment-2/finder-app is a valid Git repository)
+AESD_ASSIGNMENTS_SITE = file:///home/ahmed/Assignment-2/finder-app
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
+# Buildroot will clone this external package into $(BUILD_DIR)/aesd_assignments
 AESD_ASSIGNMENTS_DIR = $(BUILD_DIR)/aesd_assignments
 
 AESD_ASSIGNMENTS_LICENSE = GPL-2.0
@@ -20,12 +22,12 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
-	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
-	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
-	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/assignment-1-test.sh
-	$(INSTALL) -D -m 0644 $(AESD_ASSIGNMENTS_DIR)/finder-app/conf/username.txt $(TARGET_DIR)/etc/finder-app/username.txt
-	$(INSTALL) -D -m 0644 $(AESD_ASSIGNMENTS_DIR)/finder-app/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/assignment.txt
+	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
+	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/writer $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+	$(INSTALL) -D -m 0755 $(AESD_ASSIGNMENTS_DIR)/finder-test.sh $(TARGET_DIR)/usr/bin/assignment-1-test.sh
+	$(INSTALL) -D -m 0644 $(AESD_ASSIGNMENTS_DIR)/conf/username.txt $(TARGET_DIR)/etc/finder-app/username.txt
+	$(INSTALL) -D -m 0644 $(AESD_ASSIGNMENTS_DIR)/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/assignment.txt
 endef
 
 $(eval $(generic-package))
