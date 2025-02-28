3e0254e06fa2b41ef30c9fe8aba3355df3bf6ecf
AhmedAlshehhi7 <AhmedAlshehhi7@github.com>
2025-02-28 17:48:38 +0000

    Add AESD_assignments external package files with project_base

--- base_external/external.mk
+++ base_external/external.mk
@@ -4,7 +4,8 @@
 
 AESD_ASSIGNMENTS_VERSION = master
 
-# Use a file:// URL pointing to your local Assignment 3 repository
+# Use a file:// URL pointing to your local Assignment 3 repository.
+# If you eventually need to use SSH, replace with an SSH URL.
 AESD_ASSIGNMENTS_SITE = file:///home/ahmed/Assignment-2/finder-app
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
