6d9e89958ec0402a9296fe40fb16fc4b213fde24
AhmedAlshehhi7 <AhmedAlshehhi7@github.com>
2025-03-01 20:27:07 +0000

    Added submodules, external configuration, and updated AESD assignments package

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,13 +1,1 @@
-AESD_ASSIGNMENTS_VERSION = 1.0
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-AhmedAlshehhi7.git
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_DEPENDENCIES =
-
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(PKG_BUILD_DIR)/tester.sh $(TARGET_DIR)/usr/bin/tester.sh
-    $(INSTALL) -D -m 0755 $(PKG_BUILD_DIR)/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
-    $(INSTALL) -D -m 0755 $(PKG_BUILD_DIR)/writer $(TARGET_DIR)/usr/bin/writer
-endef
-
-$(eval $(generic-package))
-
+include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,28 +1,30 @@
-
 ##############################################################
 #
 # AESD-ASSIGNMENTS
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+# Reference your assignment 3 repository here.
+AESD_ASSIGNMENTS_VERSION = '7e0a42f1599780a7be0000058b4e7befc37c97d5'
+# Use the SSH URL (not HTTPS) to allow the automated build/test system to work with SSH keys.
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-AhmedAlshehhi7.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+# Build the finder application (adjust as needed based on your repository structure)
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+# Install configuration files and application utilities/scripts to the target filesystem.
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
+
